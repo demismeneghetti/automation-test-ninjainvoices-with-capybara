adf515a41b273f6ae404c1e4ac06895f20cbc97e
Fernando Papini <fernandosqa@gmail.com>
2017-04-02 11:10:27 -0300

    blank project

--- tests/features/support/hooks.rb
+++ tests/features/support/hooks.rb
@@ -10,43 +10,6 @@ Before do |feature|
 
   ## time out
   Capybara.default_max_wait_time = 5
-  ## maximixar o navegador
-  #Capybara.current_session.driver.browser.manage.window.maximize
 
-  ## Page Objects (temp)
-  @home = HomePage.new
-  @login = LoginPage.new
-  @dashboard = DashboardPage.new
-  @nav = NavbarPage.new
-  @customer = CustomerPage.new
 end
 
-Before('@save_for_remove') do
-   @name = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
-   @phone = Faker::PhoneNumber.phone_number
-   @email = Faker::Internet.free_email(@name)
-   @notes = Faker::Lorem.paragraph
-   tipos = ['Gold','Prime','Exclusivo', 'Platinum']
-
-   @customer_type = tipos.sample
-
-   @customer.new_button.click
-   @customer.save(@name, @phone, @email, @customer_type, @notes)
-end
-
-Before('@login') do
-  @login.load
-  @login.with('kato.danzo@qaninja.com.br', 'secret')
-end
-
-After('@logout') do
-  @nav.logout
-end
-
-After do |scenario|
-  file = scenario.name.gsub(/\s+/, '_').tr('/', '_')
-  path = 'reports/screenshots/'
-  screenshot = path + file.downcase + '.png'
-  page.save_screenshot(screenshot)
-  embed(screenshot, 'image/png', 'SCREENSHOT')
-end
